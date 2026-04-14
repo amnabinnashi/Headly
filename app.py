327328e3dfa0b2cd58dd69d6b30f7c5cec71b074
Amna Bin Nashi <amnabinnashi2000@gmail.com>
2026-04-14 21:48:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from scraper import WebInsightPro
+from Main import WebInsightPro
 
 app = Flask(__name__)
 
@@ -16,4 +16,4 @@ def home():
     return render_template("index.html", data=data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
