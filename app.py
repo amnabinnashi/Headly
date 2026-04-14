c49c1b047cb8cb9f62f5491d7157a81534c86f71
Amna Bin Nashi <amnabinnashi2000@gmail.com>
2026-04-14 22:27:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from Main import WebInsightPro
+import os
 
 app = Flask(__name__)
 
@@ -15,5 +16,8 @@ def home():
 
     return render_template("index.html", data=data)
 
+
+# تشغيل التطبيق (مهم لـ Render)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
